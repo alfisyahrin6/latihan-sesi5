5ffa25b4ff85d9e1b0446d5583b8904e2f95f3ab
alfisyahrin6 <alfisyahrin6@gmail.com>
2024-07-13 15:26:46 +0700

    update generator

---
+++ index.js
@@ -0,0 +1,30 @@
+let passwordLength = document.getElementById("passwordLength")
+let password = document.getElementById("password")
+let saveButton = document.getElementById("saveButton")
+
+function getPassword(){
+    const newPassword = generatePassword(passwordLength.value)
+    password.value = newPassword
+    alert('password has been generated')
+}
+
+function generatePassword(len){
+    const lowerAlphabet = "abcdefghijklmnopqrstuvwxyz"
+    const upperAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    const numeric = "0123456789"
+    const symbol = "!@#$%^&*()-_=+[]{};:',.<>/?`~"
+
+    const data = lowerAlphabet + upperAlphabet + numeric + symbol;
+    let generator = '';
+    for (let index = 0; index < len; index++) {
+        generator += data[~~(Math.random() * data.length)];
+    }
+    return generator
+}
+
+function savePassword(){
+    document.title = password.value;
+    saveButton.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(`password saya: ${document.title}`))
+    saveButton.setAttribute('download', 'MyPasswordGenerator.txt')
+    alert('berhasil disimpan')
+}
